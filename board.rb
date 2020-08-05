3d67f55c0c854071d62ec4e5c17f326498963201
Cfretwell <cfretwell97@gmail.com>
2020-08-05 16:54:48 -0600

    Have working render, board, and bomb placement

--- board.rb
+++ board.rb
@@ -2,9 +2,49 @@
 
 class Board 
 
-    def initialize(bomb_count = 9, board_size = 9) 
-        @board = Array.new(board_size) {Array.new(board_size, new.Tile(0)) }
+    require_relative 'tile.rb'
 
+    def initialize(bomb_count = 9, board_size = 9) 
+        @board = Array.new(board_size)  {Array.new(board_size) {Tile.new()}  }
+        add_bombs(bomb_count, board_size)
+
+    end
+
+    def [](x,y)
+        @board[x][y] 
+    end
+
+    def add_bombs(bomb_count, board_size)
+        bomb_locations = pick_bombs(bomb_count, board_size)
+        
+        bomb_locations.each do |pos|
+            @board[pos[0]][pos[1]].place_bomb
+        end 
+            
+    end
+
+    def pick_bombs(bomb_count, board_size)
+        arr = [*0..(board_size-1)**2].shuffle
+        arr = arr[0...bomb_count]
+        bomb_locations = []
+        arr.each do |i|
+            loc = []
+            x = i / board_size
+            y = i % board_size 
+            bomb_locations << [x,y] 
+        end
+        bomb_locations
+    end
+
+    def render 
+        puts "  " + (0...@board.length).to_a.join(" ")
+        @board.each_with_index do |row, i| 
+            print  i 
+            row.each do |t|
+                print " "+ (t.bordering_bombs).to_s 
+            end
+            puts ""
+        end
     end
 
 
@@ -13,4 +53,5 @@ end
 
 b = Board.new()
 
+b.render 
 
--- tile.rb
+++ tile.rb
@@ -2,13 +2,34 @@
 
 class Tile 
 
-    def initialize(bomb)
-        @bomb = bomb # true or false
-        @hidden = true 
+    def initialize()
+        @bomb = false # true or false
+        @hidden = false 
         @bordering_bombs = 0  
     end
 
+    def add_border_bomb
+        @bordering_bombs +=1
+    end
 
+    def bomb?
+        @bomb
+    end
+
+    def place_bomb
+        @bomb = true 
+    end
+
+    def bordering_bombs
+        return "B" if @bomb
+        return @bordering_bombs if !@hidden
+        return "X"
+    end
+
+    def flip
+        @hidden = false 
+        bordering_bombs()
+    end
 
 
 
